af31b43cf2cb3dae5f813a4c623a9da854cc13b2
Alex Karacaoglu <alexkaracaoglu@Alexs-MacBook-Pro.local>
2019-01-15 11:18:27 -0500

    Section 2.10

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
     
     // viewDidLoad executes when app's view loads
     override func viewDidLoad() {
@@ -18,25 +19,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let messageAwesome = "You are Awesome!"
-        let messageGreat = "You are Grrrrrrrr8"
-        let messageAmazing = "You are Quite Amazing"
         
-        if messageLabel.text == messageAwesome {
-            messageLabel.text = messageGreat
-            messageLabel.textColor = UIColor.purple
-            messageLabel.textAlignment = NSTextAlignment.right
+        
+        let messages = ["You are Awesome!",
+                        "You are Grrrrrrrr8",
+                        "You are Quite Amazing",
+                        "You are Pretty Cool",
+                        "This is a pretty long one so we can fix it"]
+        
+        messageLabel.text = messages[index]
+        index = index + 1
+        if index == messages.count {
+            index = 0
         }
-        else if messageLabel.text == messageGreat {
-            messageLabel.text = messageAmazing
-            messageLabel.textColor = UIColor.blue
-            messageLabel.textAlignment = NSTextAlignment.center
-        }
-        else {
-            messageLabel.text = messageAwesome
-            messageLabel.textColor = UIColor.green
-            messageLabel.textAlignment = NSTextAlignment.left
-        }
+        
+        
+//        let messageAwesome = "You are Awesome!"
+//        let messageGreat = "You are Grrrrrrrr8"
+//        let messageAmazing = "You are Quite Amazing"
+//
+//        if messageLabel.text == messageAwesome {
+//            messageLabel.text = messageGreat
+//            messageLabel.textColor = UIColor.purple
+//            messageLabel.textAlignment = NSTextAlignment.right
+//        }
+//        else if messageLabel.text == messageGreat {
+//            messageLabel.text = messageAmazing
+//            messageLabel.textColor = UIColor.blue
+//            messageLabel.textAlignment = NSTextAlignment.center
+//        }
+//        else {
+//            messageLabel.text = messageAwesome
+//            messageLabel.textColor = UIColor.green
+//            messageLabel.textAlignment = NSTextAlignment.left
+//        }
     }
     
     @IBAction func clearMessage(_ sender: UIButton) {
